80c93b3a0fc402b989812f76740463bee159f18c
Hamish Isaacs <hamisaacs@gmail.com>
2020-11-26 18:35:08 +0800

    Update html and css

---
+++ README.md
@@ -0,0 +1,72 @@
+# Generator_Password
+
+A randomly generated password where users can select any combination of numbers, symbols, uppercase and lowercase characters by selecting any checkbox combination. 
+
+Password requirements to be a minimum of 8 and a maximum of 128 chracters
+
+01. Password Generator Home Screen
+
+![GP_01](https://user-images.githubusercontent.com/19741669/100219594-2a17ee80-2f51-11eb-855f-f597cab312a8.PNG)
+
+02. If user click on button Generate Password without selecting one of the checkboxes, and alert popup will appear advising 'Please select checkbox to begin".
+
+![GP_02](https://user-images.githubusercontent.com/19741669/100219600-2dab7580-2f51-11eb-80da-8a49ee3468d2.PNG)
+
+03. Select Numbers checkbox to generate a password with numbers only.
+
+![GP_03](https://user-images.githubusercontent.com/19741669/100219607-2f753900-2f51-11eb-82b7-04cb777daea8.PNG)
+
+04. Select Symbols checkbox to generate a password with symbols only.
+
+![GP_04](https://user-images.githubusercontent.com/19741669/100219617-313efc80-2f51-11eb-8a2f-c1256415a347.PNG)
+
+05. Select Upper Case checkbox to generate a password with uppercase characters only.
+
+![GP_05](https://user-images.githubusercontent.com/19741669/100219626-33a15680-2f51-11eb-8b1d-e5122a063951.PNG)
+
+06. Select Lower Case checkbox to generate a password with lowercase characters only.
+
+![GP_06](https://user-images.githubusercontent.com/19741669/100219634-34d28380-2f51-11eb-9ee2-9eb22932a4af.PNG)
+
+07. Select ALL checkboxes (Numbers, Symbols, Upper Case and Lower Case) to generate a password with numbers, symbols, uppercase and lowercase characters.
+
+![GP_07](https://user-images.githubusercontent.com/19741669/100219642-3734dd80-2f51-11eb-8603-6aff85479766.PNG)
+
+08. Select Numbers and Symbols to generate a password with numbers and symbols.
+
+![GP_08](https://user-images.githubusercontent.com/19741669/100219651-38fea100-2f51-11eb-8895-995bbc2a82d3.PNG)
+
+09. Select Numbers and Upper Case to generate a password with numbers and uppercase characters.
+
+![GP_09](https://user-images.githubusercontent.com/19741669/100219654-3b60fb00-2f51-11eb-9ecd-86f4e8f3d84f.PNG)
+
+10. Select Numbers and Lower Case to generate a password with numbers and lowercase characters
+
+![GP_10](https://user-images.githubusercontent.com/19741669/100219657-3d2abe80-2f51-11eb-8db6-c54ce0783ffe.PNG)
+
+11. Select Symbols, Upper Case and Lower Case to generate a password with symbols, uppercase and lowercase characters
+
+![GP_11](https://user-images.githubusercontent.com/19741669/100219664-3f8d1880-2f51-11eb-8ea3-f626aaa61003.PNG)
+
+12. Select Symbols, Upper Case to generate a password with symbols and uppercase characters
+
+![GP_12](https://user-images.githubusercontent.com/19741669/100219673-40be4580-2f51-11eb-8a64-53caedc7087d.PNG)
+
+13. Select Symbols, Lower Case to generate a password with symbols and lowercase characters
+
+![GP_13](https://user-images.githubusercontent.com/19741669/100219682-43209f80-2f51-11eb-8747-803f483dc957.PNG)
+
+14. Select Upper Case and Lower Case to generate a password with uppercase and lowercase characters
+
+![GP_14](https://user-images.githubusercontent.com/19741669/100219689-44ea6300-2f51-11eb-921f-856937948a4f.PNG)
+
+15. Select Numbers, Upper Case and Lowwer Case to generate a password with numbers, uppercase and lowercase characters
+
+![GP_15](https://user-images.githubusercontent.com/19741669/100219694-46b42680-2f51-11eb-8a37-391102dab75a.PNG)
+
+16. Select Numbers, Symbols and Lower Case to generate a password with numbers, symbols and lowercase characters
+
+![GP_16](https://user-images.githubusercontent.com/19741669/100219702-487dea00-2f51-11eb-9f02-3245a100e4fb.PNG)
+
+17. The same concepts apply to generating a password anywhere between 8 and 128 characters.
+![GP_17](https://user-images.githubusercontent.com/19741669/100222438-08b90180-2f55-11eb-88e2-0f7c5c96fc7b.PNG)
---
+++ script/script.js
@@ -0,0 +1,111 @@
+// declaring varaible for Id's
+var charNum = document.getElementById("charNum");
+var numberChecked = document.getElementById("numbers");
+var symbolChecked = document.getElementById("symbols");
+var lowerCaseChecked = document.getElementById("lowercase");
+var upperCaseChecked = document.getElementById("uppercase");
+var submit = document.getElementById("generate");
+var yourPw = document.getElementById("password");
+
+
+// declaring variables for generating passwords
+var numbers = '0123456789';
+var symbols = '`~!@#$%^&*=-_<>?/+=';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+submit.addEventListener("click", function (e) {
+
+  if (!numberChecked.checked && !symbolChecked.checked && !lowerCaseChecked.checked && !upperCaseChecked.checked) {
+    // alert("To begin, please select a checkbox")
+    alert("Please select checkbox to begin!")
+  }
+
+  // this adds numeric values
+  if (numberChecked.checked) {
+    yourPw.value = password(charNum.value, numbers);
+  }
+
+  // this adds symbols characters
+  if (symbolChecked.checked) {
+    yourPw.value = password(charNum.value, symbols);
+  }
+
+  // this adds lowercase characters
+  if (lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, lowercase);
+  }
+
+  // this adds uppercase characters
+  if (upperCaseChecked.checked) {
+    yourPw.value = password(charNum.value, uppercase);
+  }
+
+  // if number and symbol have been checked
+  if (numberChecked.checked && symbolChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + symbols);
+  }
+
+  // if number and uppercase have been checked
+  if (numberChecked.checked && upperCaseChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + uppercase);
+  }
+
+  // if number and lowercase have been checked
+  if (numberChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + lowercase);
+  }
+
+  // if symbol and uppercase have been checked
+  if (symbolChecked.checked && upperCaseChecked.checked) {
+    yourPw.value = password(charNum.value, symbols + uppercase);
+  }
+
+  // if symbol and lowercase have been checked
+  if (symbolChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, symbols + lowercase);
+  }
+
+  // if uppercase and lowercase have been checked
+  if (upperCaseChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, uppercase + lowercase);
+  }
+
+  // if symbols with uppercase and lowercase have been checked
+  if (symbolChecked.checked && upperCaseChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, symbols + uppercase + lowercase);
+  }
+
+  // if number with uppercase and lowercase have been checked
+  if (numberChecked.checked && upperCaseChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + uppercase + lowercase);
+  }
+
+
+  // if number with symbols and uppercase have been checked
+  if (numberChecked.checked && symbolChecked.checked && upperCaseChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + symbols + uppercase);
+  }
+
+  // if number with symbols and lowercase have been checked
+  if (numberChecked.checked && symbolChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + symbols + lowercase);
+  }
+
+  // if number with symbols uppercase and lowercase have been checked
+  if (numberChecked.checked && symbolChecked.checked && upperCaseChecked.checked && lowerCaseChecked.checked) {
+    yourPw.value = password(charNum.value, numbers + symbols + uppercase + lowercase);
+  }
+
+});
+
+
+function password(l, characters) {
+  var pwd = '';
+  for (var i = 0; i < l; i++) {
+    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return pwd;
+  customAlert();
+}
